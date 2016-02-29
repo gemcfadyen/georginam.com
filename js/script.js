d9fef6b889bdea1c64b8333711bc3b6535e18625
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-29 16:30:15 +0000

    changes order of limit for photos

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ var feed = new Instafeed({
         userId: '360855560',
         tagName: '100days100photos',
         accessToken: '360855560.1677ed0.b889edf62bd84cb297b044f0d765c4ef',
-        resolution: 'standard_resolution',
         limit: 110,
+        resolution: 'standard_resolution',
         template: '<li class="photosFor100days"><img class="instaPhoto" src="{{image}}" /><p>{{caption}}</p><hr></li>',
         filter: function(image) {
         	return image.tags.indexOf('100days100photos') >= 0;
