dbc5696e134511833d705e6fc2cc234c689f2e90
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-29 16:23:22 +0000

    adds limit of 100 for photos page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var feed = new Instafeed({
         tagName: '100days100photos',
         accessToken: '360855560.1677ed0.b889edf62bd84cb297b044f0d765c4ef',
         resolution: 'standard_resolution',
+        limit: 100,
         template: '<li class="photosFor100days"><img class="instaPhoto" src="{{image}}" /><p>{{caption}}</p><hr></li>',
         filter: function(image) {
         	return image.tags.indexOf('100days100photos') >= 0;
